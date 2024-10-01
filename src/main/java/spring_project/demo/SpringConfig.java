6697ee1f2cde136c8363793ec57d51f02afb38df
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-10-01 15:02:31 +0900

    h2 데이터베이스 연결

--- src/main/java/spring_project/demo/SpringConfig.java
+++ src/main/java/spring_project/demo/SpringConfig.java
@@ -1,16 +1,27 @@
 package spring_project.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import spring_project.demo.repository.JdbcMemberRepository;
 import spring_project.demo.repository.MemberRepository;
-import spring_project.demo.repository.MemoryMemberRepository;
 import spring_project.demo.service.MemberService;
+
+import javax.sql.DataSource;
 
 // 자바코드로 직접 빈 등록함
 // 리포지토리 변경(db변경할 때) 손쉽게 갈아끼울 수 있음
 @Configuration
 public class SpringConfig {
-   //  memberService와 memberRepository를 Bean에 등록
+    // 설정파일을 보고 자체적으로 spring에서 bean 생성
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    //  memberService와 memberRepository를 Bean에 등록
     
     @Bean
     public MemberService memberService() {
@@ -20,7 +31,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
