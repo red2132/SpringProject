c08e6ce013abdcfbb847d53310e32261fc6b8729
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-10-01 13:33:47 +0900

    화면 연결

--- src/main/java/spring_project/demo/controller/MemberController.java
+++ src/main/java/spring_project/demo/controller/MemberController.java
@@ -2,7 +2,14 @@ package spring_project.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import spring_project.demo.domain.Member;
+import spring_project.demo.domain.MemberForm;
 import spring_project.demo.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -11,6 +18,28 @@ public class MemberController {
     @Autowired // service를 자동 주입(의존성 주입)
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public  String createForm() {
+        // 해당 화면으로 이동
+        return "members/createMemberForm";
+    }
+    
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName()); // 멤버 객체에 이름 세팅
+        
+        memberService.join(member); // 멤버 정보 등록
+        return "redirect:/"; // 홈 화면으로 리다이렉트
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
 /*
